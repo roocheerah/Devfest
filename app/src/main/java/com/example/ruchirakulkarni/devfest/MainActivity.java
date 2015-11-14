5ba8b504a354a9bb4df50910e8ca89a04460beb2
roocheerah <ruchira.kulk@gmail.com>
2015-11-14 12:49:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/ruchirakulkarni/devfest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ruchirakulkarni.devfest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.firebase.client.Firebase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Firebase.setAndroidContext(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Adding to the database...", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Firebase myFirebaseRef = new Firebase("https://glowing-heat-4595.firebaseio.com/");
+                myFirebaseRef.child("root").setValue("adding root..");
+                myFirebaseRef.child("root2").setValue(" adding root2...");
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
